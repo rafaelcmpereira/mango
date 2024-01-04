b7b464a86c42901a4a785257df6947fdbac3e20b
Rafael Martin Pereira <E3293@itsector.pt>
2024-01-04 21:22:33 +0000

    Fist database test

--- Mango.Services.CouponAPI/Data/AppDbContext.cs
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -13,6 +13,23 @@ namespace Mango.Services.CouponAPI.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon()
+                {
+                    CouponId = 1,
+                    CouponCode = "10OFF",
+                    DiscountAmount = 10,
+                    MinAmount = 20
+                },
+                new Coupon()
+                {
+                    CouponId = 2,
+                    CouponCode = "20OFF",
+                    DiscountAmount = 20,
+                    MinAmount = 40
+                }
+                );
         }
     }
 }
--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -1,5 +1,6 @@
 using Mango.Services.CouponAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,4 +30,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
