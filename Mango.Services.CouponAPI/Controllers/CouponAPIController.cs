63195248abd1bd4db86bf04d844db13601753189
Rafael Carregalo Martin Pereira <62571564+rafaelcmpereira@users.noreply.github.com>
2024-01-13 18:37:47 +0000

    Initial (#1) Created first database test
    * Initial commit
    
    * Fist database test
    
    ---------

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,71 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.Where(c => c.CouponId == id).ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+        [HttpPost]
+        public object Post(Coupon coupon)
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.Where(c => c.CouponId == coupon.CouponId).ToList();
+
+                if (objList.Count() >= 1)
+                {
+                    return null;
+                }
+                _db.Coupons.Add(coupon);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+    }
+}
